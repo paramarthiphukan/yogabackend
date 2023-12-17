18a49d0d8b6cfc7a96411b526e627e92ccd51e45
paramarthiphukan <phukanparmarthi@gmail.com>
2023-12-17 20:09:24 +0530

    ff

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URL, {
